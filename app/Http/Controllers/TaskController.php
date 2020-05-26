47d265b52bf0da9fbaaea29a703e69da681ddab3
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-27 01:00:28 +0900

    index処理を追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Task;
+
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks',['tasks' => $tasks ]);
+
+    }
 }
