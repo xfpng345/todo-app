6aea65486d5df559505099d61bb6e365e2f32e88
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-27 01:17:40 +0900

    post(store)処理を追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Task;
+use Validator;
 
 class TaskController extends Controller
 {
@@ -13,5 +14,23 @@ class TaskController extends Controller
         $tasks = Task::all();
         return view('tasks',['tasks' => $tasks ]);
 
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+        'name' => 'required|max:30',
+        ]);
+        if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);    
+        }
+
+        $task = new Task;
+        $task->title = $request->name;
+        $task->save();
+    
+        return redirect('/');
     }
 }
