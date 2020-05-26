18123652b7870cb3f364bd46157b4d2b0603f515
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-05-27 01:00:16 +0900

    ルーテイングを追加

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TaskController@index')->name('tasks,index');
+
+
