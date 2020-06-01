bc3660092206e9b70023447b31d38ac2ff0a9e96
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-06-01 23:52:59 +0900

    showページ修正

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,29 +5,33 @@
     <h3>{{ $user->name }}のページ</h3><br/>
 		<div class="col-sm-offset-2 col-sm-8">
 			<div class="panel panel-default">
-				<div class="panel-heading">
-				<h4>作成</h4>
-				</div>
-				<div class="panel-body">
-					@include('commons.errors')
-					<form action="/task" method="POST" class="form-horizontal">
-						{{ csrf_field() }}
-						<div class="form-group">
-							<label for="task-name" class="col-sm-3 control-label"></label>
-							<div class="col-sm-6">
-								<input type="text" name="title" id="task-name" class="form-control" value="{{ old('task') }}">
-							</div>
-						</div>
-						<div class="form-group">
-							<div class="col-sm-offset-3 col-sm-6">
-								<button type="submit" class="btn btn-primary">
-									<i class="fa fa-plus"></i>トレーニングを追加する
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
+        @auth
+          @if( ( $user->id ) == ( Auth::user()->id ) )
+          <div class="panel-heading">
+				    <h4>作成</h4>
+				  </div>
+            <div class="panel-body">
+              @include('commons.errors')
+              <form action="/task" method="POST" class="form-horizontal">
+                {{ csrf_field() }}
+                <div class="form-group">
+                  <label for="task-name" class="col-sm-3 control-label"></label>
+                  <div class="col-sm-6">
+                    <input type="text" name="title" id="task-name" class="form-control" value="{{ old('task') }}">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-primary">
+                      <i class="fa fa-plus"></i>トレーニングを追加する
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          @endif
+				@endauth
 				<div class="panel panel-default">
 					<div class="panel-heading">
 						トレーニング一覧
